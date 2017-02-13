0fb27874f3cfb062fa2456c2653811cc0a2ad8a1
guolicheng <dhf0710@163.com>
2017-02-13 16:12:10 +0800

    showNotebooks function

--- src/main/webapp/js/notebook.js
+++ src/main/webapp/js/notebook.js
@@ -2,6 +2,24 @@ function showNotebooksAction(){
 		var url = 'notebook/list.do';
 		var param = {'userId':getCookie('userId')};
 		$.post(url,param,function(result){
+			if(result.state==SUCCESS){
+				var notebooks = result.data;
+				model.updateNotebookView(notebooks);
+			}
 			
 		});
+}
+
+model.updateNotebookView= function(notebooks){
+	this.notebooks = notebooks;
+	var ul = $('#notebook');
+	var template = '<li class="online">'+
+						'<a><i class="fa fa-book" title="online" rel="tooltip-bottom"/>'+
+						'</i>notebook.name</a>'+
+				    '</li>';
+	for(var i=0;i<this.notebooks.length;i++){
+		var notebook = this.notebooks[i];
+		var li = template.replace('notebook.name',notebook.name);
+		ul.append(li);
+	}
 }
