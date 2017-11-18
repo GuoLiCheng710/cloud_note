b1765cf1dac163b2e1cc05aac9d24cfc3d831019
Silence <dhf0710@163.com>
2017-11-18 20:30:40 +0800

    Update Util.java

--- src/main/java/com/zrar/note/util/Util.java
+++ src/main/java/com/zrar/note/util/Util.java
@@ -3,7 +3,7 @@ package com.zrar.note.util;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class Util {
-	private static final String salt = "tedu.cn是一家培训机构";
+	private static final String salt = "i'm dinghongfei";
 	public static String salMD5(String data){
 		return DigestUtils.md5Hex(data+salt);
 	}
